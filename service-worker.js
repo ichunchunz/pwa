5ff0abc74e165245e5b6bc1fa2d52ed3ab74f506
ichunchunz <31593164+ichunchunz@users.noreply.github.com>
2019-07-26 11:21:19 +0800

    1

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'index.html',
+  './index.html',
   './', // Alias for index.html
   './css/styles.css',
 ];
